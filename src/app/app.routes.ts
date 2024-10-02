d02b12a6e285df4480bc4beb334ba8f9338fe63b
Guillaume Savary <svy.guillaume@gmail.com>
2024-10-02 18:55:50 +0200

    Mise en place, Router, styling, headerComponent, Presentation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProjetsListComponent } from './projets-list/projets-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent},
+    { path: 'projets', component: ProjetsListComponent}
+];
---
+++ src/app/projets-list/projets-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projets-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './projets-list.component.html',
+  styleUrl: './projets-list.component.scss'
+})
+export class ProjetsListComponent {
+
+}
