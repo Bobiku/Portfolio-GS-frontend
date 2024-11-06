53b08b29ad20262522c958420a77ee3ab8e62296
Guillaume Savary <savaryguillaume@proton.me>
2024-11-06 13:22:37 +0100

    Install Lucide, change body color, fix vulnerabilities

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProjetsListComponent } from "../projets-list/projets-list.component";
 import { ContactComponent } from "../contact/contact.component";
+import { Brush, Code, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink, ProjetsListComponent, ContactComponent],
+  imports: [RouterLink, ProjetsListComponent, ContactComponent, LucideAngularModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
-
+  readonly Brush = Brush;
+  readonly Code = Code
 }
