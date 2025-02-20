62f9d7d265b0b73fba478d7816ea62011735fd9f
Guillaume Savary <savaryguillaume@proton.me>
2025-02-20 15:24:36 +0100

    Ajouter des liens pour les blocks et la multisélection de propriétés d'annotation pour les blocks

--- src/app/components/formatted-text/formatted-text.component.ts
+++ src/app/components/formatted-text/formatted-text.component.ts
@@ -6,15 +6,39 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <ng-container [ngSwitch]="true">
-      <strong *ngSwitchCase="segment?.annotations?.bold">{{ segment?.plain_text }}</strong>
-      <em *ngSwitchCase="segment?.annotations?.italic">{{ segment?.plain_text }}</em>
-      <u *ngSwitchCase="segment?.annotations?.underline">{{ segment?.plain_text }}</u>
-      <s *ngSwitchCase="segment?.annotations?.strikethrough">{{ segment?.plain_text }}</s>
-      <span *ngSwitchDefault>{{ segment?.plain_text }}</span>
+    <ng-container *ngIf="segment">
+      <a *ngIf="segment.href" [href]="segment.href" target="_blank">
+        <span [ngStyle]="getStyles()">{{ segment.plain_text }}</span>
+      </a>
+      <span *ngIf="!segment.href" [ngStyle]="getStyles()">{{ segment.plain_text }}</span>
     </ng-container>
   `
 })
 export class FormattedTextComponent {
   @Input() segment: any;
+  @Input() blockType: string = '';
+
+  get shouldApplyStyles(): boolean {
+    return this.blockType === 'paragraph' || 
+           this.blockType === 'bulleted_list_item' ||
+           this.blockType === 'numbered_list_item';
+  }
+
+  getStyles() {
+    if (!this.segment?.annotations || !this.shouldApplyStyles) return {};
+
+    return {
+      'font-weight': this.segment.annotations.bold ? 'bold' : 'normal',
+      'font-style': this.segment.annotations.italic ? 'italic' : 'normal',
+      'text-decoration': this.getTextDecoration(),
+      'color': this.segment.annotations.color !== 'default' ? this.segment.annotations.color : 'inherit'
+    };
+  }
+
+  private getTextDecoration(): string {
+    const decorations: string[] = [];
+    if (this.segment.annotations.underline) decorations.push('underline');
+    if (this.segment.annotations.strikethrough) decorations.push('line-through');
+    return decorations.join(' ');
+  }
 }
