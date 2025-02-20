d92644c219bd16331a18dabb5da48de22403a7e6
Guillaume Savary <savaryguillaume@proton.me>
2025-02-20 15:56:41 +0100

    Actualisation automatique de l'année du copyright

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LucideAngularModule, FileUser, Mail, Linkedin } from 'lucide-angular';
 
 @Component({
@@ -7,8 +7,21 @@ import { LucideAngularModule, FileUser, Mail, Linkedin } from 'lucide-angular';
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-  readonly Linkedin = Linkedin;
-  readonly FileUser = FileUser;
-  readonly Mail = Mail;
+export class FooterComponent implements OnInit {
+    readonly Linkedin = Linkedin;
+    readonly FileUser = FileUser;
+    readonly Mail = Mail;
+
+    startYear = 2024;
+    currentYear!: number;
+
+    ngOnInit() {
+        this.currentYear = new Date().getFullYear();
+    }
+
+    get copyrightYears(): string {
+        return this.currentYear > this.startYear 
+            ? `${this.startYear}-${this.currentYear}`
+            : this.startYear.toString();
+    }
 }
