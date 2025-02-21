e74a7871d3146aea39d25a78aaddf17852619467
Guillaume Savary <savaryguillaume@proton.me>
2025-02-21 18:23:28 +0100

    fix link crawlable

--- src/app/components/projet-card/projet-card.component.ts
+++ src/app/components/projet-card/projet-card.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Projet } from '../../projets/models/projet';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { LucideAngularModule, ChevronLeft, ChevronRight } from 'lucide-angular';
 import { OptimizedImageComponent } from "../../shared/components/optimized-image/optimized-image.component";
 
 @Component({
     selector: 'app-projet-card',
     standalone: true,
-    imports: [LucideAngularModule, OptimizedImageComponent],
+    imports: [LucideAngularModule, OptimizedImageComponent, RouterLink],
     templateUrl: './projet-card.component.html',
     styleUrl: './projet-card.component.scss'
 })
@@ -17,10 +17,4 @@ export class ProjetCardComponent {
 
     readonly ChevronLeft = ChevronLeft;
     readonly ChevronRight = ChevronRight;
-
-    constructor(private router: Router) {}
-
-    navigateToProject(projetId: string): void {
-        this.router.navigate(['/projets/', projetId]);
-    }
 } 
